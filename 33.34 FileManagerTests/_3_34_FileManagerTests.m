999b5d93af710189301d771ccbd19dd81fa886ce
Nikita Semakov <n.semakov@yahoo.com>
2015-08-15 18:26:29 +0300

    Initial Commit

---
+++ 33.34 FileManagerTests/_3_34_FileManagerTests.m
@@ -0,0 +1,40 @@
+//
+//  _3_34_FileManagerTests.m
+//  33.34 FileManagerTests
+//
+//  Created by Admin on 15.08.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface _3_34_FileManagerTests : XCTestCase
+
+@end
+
+@implementation _3_34_FileManagerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
